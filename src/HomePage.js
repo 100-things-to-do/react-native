bb6db21f7d15dd410847f3f02624fe3a4ff7c7bc
doruksahin <doruk.sahin@inavitas.com>
2022-03-14 17:37:17 +0300

    adds carousel to homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,23 @@
-import { StyleSheet, Text, View, Animated, Button } from 'react-native';
+import { StyleSheet, Text, View, Animated, Button, Image, Dimensions } from 'react-native';
+import Carousel from 'react-native-banner-carousel';
 
+const BannerWidth = Dimensions.get('window').width;
+const BannerHeight = 100;
+const images = [
+    "https://reactjs.org/logo-og.png",
+    "https://reactjs.org/logo-og.png",
+    "https://reactjs.org/logo-og.png"
+];
 
 export default function HomePage({ navigation }) {
+    function renderPage(image, index) {
+        return (
+            <View key={index}>
+                <Image style={{ width: BannerWidth, height: BannerHeight }} source={{ uri: image }} />
+            </View>
+        );
+    }
+
     return (
         <View>
             <Button
@@ -12,6 +28,18 @@ export default function HomePage({ navigation }) {
                 onPress={() => { navigation.navigate('ProfilePage'); }}
                 title="ProfilePage"
             />
+            <View>
+                <Carousel
+                    autoplay
+                    autoplayTimeout={5000}
+                    loop
+                    index={0}
+                    pageSize={BannerWidth}
+                    useNativeDriver={false}
+                >
+                    {images.map((image, index) => renderPage(image, index))}
+                </Carousel>
+            </View>
         </View>
     )
 }
