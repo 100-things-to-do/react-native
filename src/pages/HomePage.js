3e2bab7f4bc67dfef9332ea8ba080a949d074acf
doruksahin <doruk.sahin@inavitas.com>
2022-03-15 12:41:04 +0300

    deleted unnecessary css from HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View, Animated, Button, Image, Dimensions } from 'react-native';
-import CustomCarousel from '../components/carousel'
+import Carousel from '../components/Carousel'
 const BannerWidth = Dimensions.get('window').width;
 const BannerHeight = 100;
 const images = [
@@ -7,25 +7,7 @@ const images = [
     "https://reactjs.org/logo-og.png",
     "https://reactjs.org/logo-og.png"
 ];
-const responsive = {
-    superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5
-    },
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2
-    },
-    mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1
-    }
-};
+
 
 function Achievements() {
     function getAchievements() {
@@ -43,14 +25,6 @@ function Achievements() {
 }
 
 export default function HomePage({ navigation }) {
-    function renderPage(image, index) {
-        return (
-            <View key={index}>
-                <Image style={{ width: BannerWidth, height: BannerHeight }} source={{ uri: image }} />
-            </View>
-        );
-    }
-
     return (
         <View style={styles.pageContainer}>
             <View style={styles.topImageContainer}>
@@ -60,25 +34,17 @@ export default function HomePage({ navigation }) {
                 Challange yourself with {"\n"} Activify
             </Text>
 
+            <View style={styles.midImageContainer}>
+                <Image source={{ uri: images[0] }} style={styles.topImage} />
+            </View>
 
 
             <Text>
                 Popular Challanges
             </Text>
-            <View style={styles.popularChallangesContainer}>
-                <Image source={{ uri: images[0] }} style={styles.popularChallangeLeftImage} />
-                <Image source={{ uri: images[0] }} style={styles.popularChallangeRightImage} />
-
+            <View style={styles.carouselContainer}>
+                <Carousel />
             </View>
-            <CustomCarousel></CustomCarousel>
-            <Button
-                onPress={() => { navigation.navigate('CategoriesPage'); }}
-                title="CategoriesPage"
-            />
-            <Button
-                onPress={() => { navigation.navigate('ProfilePage'); }}
-                title="ProfilePage"
-            />
         </View>
 
 
@@ -93,29 +59,20 @@ const styles = StyleSheet.create({
     },
     topImageContainer: {
         flex: 0.4,
-        margin: 0,
-        backgroundColor: 'red',
-        alignItems: 'center',
     },
     topImage: {
         width: '100%',
         height: '100%',
-        alignContent: 'center',
+    },
+    midImageContainer: {
+        flex: 0.2,
+        marginRight: '5%',
+        marginLeft: '5%',
     },
     centeredText: {
         textAlign: 'center'
     },
-    popularChallangesContainer: {
-        flex: 0.2,
-        alignContent: 'space-between',
-        flexDirection: 'row',
-    },
-    popularChallangeLeftImage: {
-        flex: 0.5,
-        marginRight: '5%',
-    },
-    popularChallangeRightImage: {
-        flex: 0.5,
-        marginLeft: '5%',
-    },
+    carouselContainer: {
+        flex: 0.4,
+    }
 });
