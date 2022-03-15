46d05d04210c6b162fd1ed1d3c3134bcf48a3cfa
doruksahin <doruk.sahin@inavitas.com>
2022-03-16 00:51:11 +0300

    unnecessary css deletion.

--- src/components/carousel/style.js
+++ src/components/carousel/style.js
@@ -9,50 +9,16 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     backgroundColor: '#FFFFFF',
   },
-  navBar: {
-    width: '100%',
-    height: 70,
-    backgroundColor: '#0D0D0D',
-    alignContent: 'center',
-    justifyContent: 'center'
-  },
-  navBarTitle: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: "bold",
-    alignSelf: 'center'
-  },
   scrollViewContainerStyle: {
     alignContent: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    height: imageHeight + 50
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%'
-  },
-  button: {
-    height: 50,
-    width: 100,
-    backgroundColor: '#0D0D0D',
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  disable: {
-    opacity: 0.5
+    height: 50
   },
   cardCountContainer: {
     paddingBottom: 20
   },
-  countText:{
+  countText: {
     fontSize: 26
   }
 });
