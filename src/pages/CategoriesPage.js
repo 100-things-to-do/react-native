274edd01a30371cb7d3569f65b613e62dc2b385a
doruksahin <doruk.sahin@inavitas.com>
2022-03-17 15:30:25 +0300

    refactor Categories page

--- src/pages/CategoriesPage.js
+++ src/pages/CategoriesPage.js
@@ -1,80 +1,88 @@
 import React from "react";
 import { StyleSheet, View, Text, FlatList, ScrollView, Dimensions } from "react-native";
+import Border from '../components/Border';
 
-const SQUARE_MARGIN = Dimensions.get('window').width / 20
-const SQUARE_SIDE_LENGTH = 8 * Dimensions.get('window').width / 20
-const SQUARE_BORDER_COLOR = "#fff"
+const SQUARE_MARGIN = Dimensions.get('window').width / 25
+const SQUARE_SIDE_LENGTH = 8 * Dimensions.get('window').width / 22
+const SQUARE_BORDER_COLOR = "red"
+const ACTIVITY_TEXT_COLOR = 'white'
 const numColumns = 2;
 const BACKGROUND_COLOR = "#7CA1B4"
-const CATEGORY_MARGIN_BOTTOM = '5%'
 
-const data = [
-    {
-        name: 'Cars',
-        letters: ['A', 'B', 'C']
-    },
-    {
-        name: 'Homes',
-        letters: ['D', 'E', 'F']
-    },
-    {
-        name: 'Guns',
-        letters: ['H', 'I', 'J']
-    }
-];
+const { data } = require('../mock/categoriesData')
 
 
 
-const renderSquares = ({ item, index }) => {
+const renderActivity = ({ item: activity, index }) => {
+    return (
+        <View>
+            <View style={styles.square}>
+                <Text style={styles.text}>{activity.name}</Text>
+            </View>
+            <Text style={styles.activityProgress}>{activity.progress}</Text>
+
+        </View>
+    )
+}
+
+const renderCategory = ({ item: category, index }) => {
     return (
         <View style={styles.categoryContainer}>
-            <Text>{item.name}</Text>
+            <Border />
+            <Text style={styles.categoryTitleText}>{category.name}</Text>
             <FlatList
-                data={item.letters}
-                renderItem={({ item, index }) => {
-                    return (
-                        <View style={styles.square}>
-                            <Text style={styles.text}>{item}</Text>
-                        </View>
-                    )
-                }}
+                data={category.activities}
+                renderItem={renderActivity}
                 numColumns={numColumns}
             />
+
         </View>
     );
 };
 export default function CategoriesPage() {
-
-
     return (
-        <FlatList style={styles.container}
-            data={data}
-            renderItem={renderSquares}
-        />
-
-
+        <View style={styles.mainContainer}>
+            <FlatList style={styles.mainFlatListContainer}
+                data={data}
+                renderItem={renderCategory}
+            />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    mainContainer: {
         backgroundColor: BACKGROUND_COLOR,
+    },
+    mainFlatListContainer: {
+
+        margin: '5%'
     },
     square: {
         borderColor: SQUARE_BORDER_COLOR,
         borderWidth: 1,
         width: SQUARE_SIDE_LENGTH,
         height: SQUARE_SIDE_LENGTH,
-        margin: SQUARE_MARGIN,
+        marginLeft: SQUARE_MARGIN,
+        marginRight: SQUARE_MARGIN,
+        marginTop: SQUARE_MARGIN,
         justifyContent: 'center',
         alignItems: "center",
+        backgroundColor: 'black'
     },
     text: {
-        color: SQUARE_BORDER_COLOR,
+        color: ACTIVITY_TEXT_COLOR,
         fontSize: 18,
         fontWeight: "bold",
     },
     categoryContainer: {
-        marginBottom: CATEGORY_MARGIN_BOTTOM,
+        marginBottom: '5%',
+    },
+    categoryTitleText: {
+        marginTop: '2.5%',
+        marginLeft: '5%'
+    },
+    activityProgress: {
+        marginLeft: '10%'
     }
 });
