7912c969682662ab0ae544cd30f3419b14a9caa5
doruk.sahin <doruk.sahin@trendyol.com>
2022-08-14 15:42:30 +0300

    adds localization

--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -2,10 +2,22 @@ import en from './translations/en.json';
 import tr from './translations/tr.json';
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import * as Localization from 'expo-localization';
 
-i18next.use(initReactI18next)
+const languageDetector = {
+    type:'languageDetector',
+    async: true,
+    detect: (callback)=>{
+      return callback(Localization.locale.split("-")[0])
+    },
+    init:()=>{},
+    cacheUserLanguage:()=>{}
+}
+i18next
+    .use(languageDetector)
+    .use(initReactI18next)
 .init({
-    lng: 'en',
+    fallbackLng: 'en',
     resources:{
         en,
         tr
